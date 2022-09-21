a2893996e452c6598a6446ab8839fc179edb1f51
Daniel Pedrozo <daniel.sacy@gmail.com>
2022-09-21 10:09:43 -0300

    Task_MOD8

---
+++ MOD08/src/CalcAVG.java
@@ -0,0 +1,12 @@
+/**
+ * @created 21/09/2022 - 7:49 AM
+ * @project MOD08
+ * Author Daniel Pedrozo
+ */
+public class CalcAVG {
+    public static void main(String args[]) {
+        DefAVG defavg = new DefAVG();
+        float task = defavg.returnAVG();
+        System.out.println(task);
+    }
+}
---
+++ MOD08/src/DefAVG.java
@@ -0,0 +1,15 @@
+/**
+ * @created 21/09/2022 - 7:50 AM
+ * @project MOD08
+ * Author Daniel Pedrozo
+ */
+public class DefAVG {
+    private float grade1 = 8;
+    private float grade2 = 7;
+    private float grade3 = 10;
+    private float grade4 = 5;
+
+    public float returnAVG() {
+        return (grade1+grade2+grade3+grade4) / 4;
+    }
+}
